1185d0cb9cbb33dfea99df51da5bdaafe8cf7871
phptheme <dev@getphptheme.com>
2020-03-09 22:30:56 +0300

    n/a

--- Views/post.php
+++ Views/post.php
@@ -1,5 +1,3 @@
-<h1><?= $title;?></h1>
-
 <p class="blog-post-meta"><?= $created;?></p>
 
 <?= $text;?>
--- Views/posts-item.php
+++ Views/posts-item.php
@@ -1,4 +1,5 @@
 <div class="blog-post">
+<h3><a href="<?= $url;?>"><?= $title;?></a></h3>
 <p class="blog-post-meta"><?= $created;?></p>
 <p><?= $description;?></p>
 </div>
